f1f2b0d93316a1fdacf0f44e6f563b7e3ef3ed8b
Carlos Diaz <charliedn@gmail.com>
2020-11-04 13:21:15 +0000

    Added tetrominoes fall, limits, and move left function

--- app.js
+++ app.js
@@ -9,37 +9,119 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
-  const width = 10;
   let squares = Array.from(document.querySelectorAll('.grid div'));
   const scoreDisplay = document.querySelector('#score');
   const startBtn = document.querySelector('#start-button')
   console.log(squares);
 
   let currentPosition = 4;
+  let currentRotation = 0;
 
-
+  const width = 10;
   // The tetrominoes. Each contains four arrays with its rotations
+
   const lTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
+    [1, 2, width + 1, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, 2],
+    [1, width + 1, width * 2, width * 2 + 1],
     [width, width * 2, width * 2 + 1, width * 2 + 2]
   ];
 
+  // const lTetromino = [
+  //   [1, width + 1, width + 2, width * 2 + 1],
+  //   [width, width + 1, width + 2, width * 2 + 1],
+  //   [1, width, width + 1, width * 2 + 1],
+  //   [width, width + 1, width * 1 + 2, width * 2 + 1],
+  // ];
+
 
   const tetrominoes = [lTetromino];
-  let current = tetrominoes[0][0];
+  let random = Math.floor(Math.random() * tetrominoes.length);
+  let current = tetrominoes[random][currentRotation];
 
-  console.log(tetrominoes);
 
   draw = () => {
+    // Draw 1st rotation
     current.forEach(index => {
-      squares[currentPosition + index].classList.add('tetromino');
-      console.log(squares[currentPosition + index]);
+      squares[index + currentPosition].classList.add('tetromino');
+      // squares[index + currentPosition].innerHTML = index;
+      // console.log(squares[currentPosition + index]);
+      console.log(currentPosition);
     })
   }
 
-  draw();
+  undraw = () => {
+    current.forEach(index => {
+      squares[index + currentPosition].classList.remove('tetromino');
+      // console.log("Undrawing");
+    })
+
+  }
+
+  freeze = () => {
+    const tetrominoesInDeadZone = current.some(
+      index =>
+        squares[currentPosition + index + width]
+          .classList
+          .contains('taken')
+    );
+
+    if (tetrominoesInDeadZone) {
+      current.forEach(index =>
+        squares[currentPosition + index].classList.add('taken')
+      );
+      rnd = Math.floor(Math.random() * tetrominoes.length);
+      current = tetrominoes[rnd][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
+
+
+  // Moves tetromino down every t 
+  moveDown = () => {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  };
+
+
+  function control(e) {
+    switch (e.keyCode) {
+      case 37:
+        moveLeft();
+        break;
+
+      case 39:
+        moveRight();
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  document.addEventListener('keyup', control);
+  // 1000 default
+  timerId = setInterval(moveDown, 400);
+  moveDown();
+
+  moveLeft = () => {
+    undraw();
+    const isAtLeftEdge = current.some(
+      index => (currentPosition + index) % width === 0
+    )
+
+    // if(!isAtLeftEdge) currentPosition -= 1;
+    isAtLeftEdge ? '' : currentPosition -= 1;
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1;
+    }
+    draw();
+  };
+
 
 
   // Other shapes from Ania's repo
